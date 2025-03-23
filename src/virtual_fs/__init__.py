22b46b32100e771921c587ed55092773c1c73129
zackees <z@zackees.com>
2025-03-22 17:46:02 -0700

    feat: add mount and find_conf_file methods to Vfs class

--- src/virtual_fs/__init__.py
+++ src/virtual_fs/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from typing import Union
 
-from rclone_api import Config, FSPath, RealFS, RemoteFS  # Filesystem utilities
+from rclone_api import Config, FSPath, Mount, RealFS, RemoteFS  # Filesystem utilities
 
 RcloneConfig = Union[str, Path, Config, None]
 
@@ -10,7 +10,7 @@ class Vfs:
 
     @staticmethod
     def begin(src: str | Path, rclone_conf: RcloneConfig = None) -> FSPath:
-        """Begins a new session with the given path."""
+        """Begins a new session with the given path, with operator on this object to ensure proper cleanup."""
         vfs: RemoteFS | RealFS
         if Vfs.looks_like_remote_path(src):
             vfs = Vfs.create_remote(src=src, rclone_conf=rclone_conf)
@@ -29,6 +29,44 @@ class Vfs:
         return fs
 
     @staticmethod
+    def mount(
+        src: str,
+        mount_dst: Path,
+        rclone_conf: RcloneConfig = None,
+        allow_writes: bool | None = False,
+        transfers: int | None = None,  # number of writes to perform in parallel
+        use_links: bool | None = None,
+        vfs_cache_mode: str | None = None,
+        verbose: bool | None = None,
+        cache_dir: Path | None = None,
+        cache_dir_delete_on_exit: bool | None = None,
+        log: Path | None = None,
+        other_args: list[str] | None = None,
+    ) -> Mount:
+        from rclone_api import Rclone
+
+        rclone_conf = rclone_conf or Vfs.find_conf_file()
+        if rclone_conf is None:
+            raise ValueError("rclone_conf not found")
+        if isinstance(rclone_conf, str):
+            rclone_conf = Config(text=rclone_conf)
+        assert rclone_conf is not None
+        mount: Mount = Rclone(rclone_conf).mount(
+            src=src,
+            outdir=mount_dst,
+            allow_writes=allow_writes,
+            transfers=transfers,
+            use_links=use_links,
+            vfs_cache_mode=vfs_cache_mode,
+            verbose=verbose,
+            cache_dir=cache_dir,
+            cache_dir_delete_on_exit=cache_dir_delete_on_exit,
+            log=log,
+            other_args=other_args,
+        )
+        return mount
+
+    @staticmethod
     def create_local() -> RealFS:
         fs = RealFS()
         return fs
@@ -39,6 +77,21 @@ class Vfs:
 
         path_str = Path(path).as_posix()
         return ":" in path_str and not re.match(r"^[a-zA-Z]:[/\\]", path_str)
+
+    @staticmethod
+    def find_conf_file() -> Path | None:
+        import os
+
+        # if os.environ.get("RCLONE_CONFIG"):
+        #     return Path(os.environ["RCLONE_CONFIG"])
+        # return None
+        # rclone_conf = rclone_conf or Path.cwd() / "rclone.conf"
+
+        if os.environ.get("RCLONE_CONFIG"):
+            return Path(os.environ["RCLONE_CONFIG"])
+        if (conf := Path.cwd() / "rclone.conf").exists():
+            return conf
+        return None
 
 
 __all__ = ["FSPath", "RemoteFS"]
